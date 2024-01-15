ec4250a5f33155731c5b955941c370d28f09a313
Ezequiel Fernandez Cariaga <dia.fernandezcariaga.ezequiel@gmail.com>
2024-01-15 18:12:20 -0300

    new components tests

---
+++ 04-gif-expert-app/tests/components/GifGrid.test.jsx
@@ -0,0 +1,7 @@
+describe('Pruebas en <GifGrid />', () => {
+
+    test('Debe', () => {
+
+        
+    })
+})
--- 04-gif-expert-app/tests/helpers/getGifs.test.js
+++ 04-gif-expert-app/tests/helpers/getGifs.test.js
@@ -1,5 +1,17 @@
-describe('Pruebas en getGifs', () => {
-    test('', () => {
-        
+import { getGifs } from "../../src/helpers/getGifs"
+
+describe('Pruebas en getGifs()', () => {
+
+    test('Debe retornar un arreglo de gifs', async() => {
+
+        const gifs = await getGifs('Dragon Ball Z');
+        // console.log(gifs);
+
+        expect(gifs.length).toBeGreaterThan(0);
+        expect(gifs[0]).toEqual({
+            id: expect.any(String),
+            title: expect.any(String),
+            url: expect.any(String),
+        })
     })
 })
