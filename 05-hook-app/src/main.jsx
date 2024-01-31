60105c696d05d90700ec2fd146b7f1e3a37c5c6f
Pilar Girolami <pilargirolami@MacBook-Air-de-Pilar.local>
2024-01-30 21:06:05 -0300

    useState with custom hook + useEffect explanation & custom hook

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HooksApp } from './HooksApp'
 
+// import { CounterApp } from './01-useState/CounterApp'
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+// import { SimpleForm } from './02-useEffect/SimpleForm'
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+
+
 import './index.css'
-import { CounterApp } from './01-useState/CounterApp'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterApp />
+    <FormWithCustomHook />
   </React.StrictMode>,
 )
